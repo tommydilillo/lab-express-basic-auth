88456a07107b956a4ab49701f617598ac6b91b07
Tommy DiLillo <tommydilillo@gmail.com>
2018-07-08 18:27:47 -0400

    iteration 1 signup done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,57 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const User = require("../models/user");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ username: username })
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists"
+        });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save().then(user => {
+        res.redirect("/");
+      });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
 });
 
 module.exports = router;
